57909373acfd03563c1ebaff9eb5f0ee46afe318
Anne-Sophie SIMON <a-sophie.simon@live.fr>
2023-05-16 12:39:43 -0400

    mern-project

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className="loadingSpinnnerContainer">Spinner
+    <div className="loadingSpinner"></div>
+    </div>
+  )
+}
+
+export default Spinner;
